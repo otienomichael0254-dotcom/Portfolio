434726773998bc5aa2e5e1eec244d92d637dffc4
Michael <otienomichael0254@gmail.com>
2026-06-11 17:18:18 +0300

    Fix EmailJS template ID and add email parameter to auto-reply to resolve 422 error

--- emailjs.config.js
+++ emailjs.config.js
@@ -1,6 +1,6 @@
 const EMAILJS_CONFIG = {
   publicKey: '5s8BwvdPKSrj6jufp',
   serviceId: 'service_35gqr5w',
-  templateId: 'template_6dyuk08v',
+  templateId: 'template_6dyuk08',
   autoReplyTemplateId: 'template_b6fhvwu',
 };
